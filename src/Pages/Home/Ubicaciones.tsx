43431416fe91d27392fe44b240060553329985bb
AlvaroTorres0 <alvaro.espindola.torres@gmail.com>
2024-05-26 17:13:01 -0600

    feat: Update home page layout and add LocationCard component

--- src/Pages/Home/Ubicaciones.tsx
+++ src/Pages/Home/Ubicaciones.tsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import { useLocations } from '../../hooks/useLocations'
+import LocationCard from '../../components/LocationCard'
 
 const Ubicaciones: React.FC = () => {
+  const { locations, loading, error, handleNextLocation, handlePrevLocation, currentLocation } = useLocations()
+
   return (
-        <h2>Ubicaciones</h2>
+        <section className='flex flex-col gap-8'>
+          <div>
+            <h2 className='font-principal-black primary-text-color text-5xl'>Todas las ubicaciones</h2>
+            <p className='font-principal-light text-lg'>Selecciona tus ubicaciones favoritas</p>
+          </div>
+          <div className="carousel w-full flex">
+            <div id={'slide1'} className="carousel-item relative">
+                {
+                    locations.length > 0
+                      ? <>
+                          <LocationCard
+                            id={locations[currentLocation].id}
+                            name={locations[currentLocation].name}
+                            type={locations[currentLocation].type}
+                            dimension={locations[currentLocation].dimension}
+                          />
+                          <div className="absolute flex justify-between transform -translate-y-1/2 left-0 right-5 top-[90%] lg:top-[80%]">
+                            <button onClick={() => handlePrevLocation()} className="btn btn-circle">❮</button>
+                            <button onClick={() => handleNextLocation()}className="btn btn-circle">❯</button>
+                          </div>
+                        </>
+                      : (
+                        <p>No hay</p>
+                        )
+                }
+            </div>
+        </div>
+
+        </section>
   )
 }
 
