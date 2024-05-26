72c741de637807d886c8efb3b3ac16a3fee57837
AlvaroTorres0 <alvaro.espindola.torres@gmail.com>
2024-05-26 17:12:22 -0600

    refactor: Remove unused properties from Location interface

--- src/d.ts
+++ src/d.ts
@@ -24,9 +24,6 @@ export interface Location {
     readonly name: string;
     readonly type: string;
     readonly dimension: string;
-    readonly residents: string[];
-    readonly url: string;
-    readonly created: Date;
 }
 
 export interface APIResponse {
