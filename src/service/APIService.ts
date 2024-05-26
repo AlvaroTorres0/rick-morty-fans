8c127fe545970dd73a5a3290091971d99806189a
AlvaroTorres0 <alvaro.espindola.torres@gmail.com>
2024-05-26 15:05:11 -0600

    feat: Update error message in APIService

--- src/service/APIService.ts
+++ src/service/APIService.ts
@@ -6,7 +6,7 @@ export async function getApiData ({ url }: FetchParams) {
   const response = await fetch(url)
   const data = await response.json()
   if (data.error) {
-    throw new Error('No se encontraron personajes')
+    throw new Error('Error fetching data from API')
   }
   return data
 }
