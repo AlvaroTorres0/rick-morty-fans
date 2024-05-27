258901ad1588185127932d1a8d568b65f75b7983
AlvaroTorres0 <alvaro.espindola.torres@gmail.com>
2024-05-26 19:34:47 -0600

    refactor: Remove SupabaseProvider from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import { SupabaseProvider } from './context/SupabaseClientContext'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <SupabaseProvider>
         <App />
-      </SupabaseProvider>
     </BrowserRouter>
 
   </React.StrictMode>
