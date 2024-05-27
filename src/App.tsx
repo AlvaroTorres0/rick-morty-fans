035daa1e9bf18460153aa6ed8de0ea90f55df48f
AlvaroTorres0 <alvaro.espindola.torres@gmail.com>
2024-05-26 21:01:17 -0600

    feat: Add AuthContextProvider for authentication context in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css'
 import React from 'react'
 import RountingHandler from './RoutingHandler'
+import { AuthContextProvider } from './context/AuthContext'
 
 const App: React.FC = () => {
   return (
-    <RountingHandler></RountingHandler>
+    <AuthContextProvider>
+      <RountingHandler></RountingHandler>
+    </AuthContextProvider>
   )
 }
 
