30fe26902755e2ef2c8e98683aa1a013ed9980f2
AlvaroTorres0 <alvaro.espindola.torres@gmail.com>
2024-05-27 10:07:29 -0600

    refactor: Update APIService to include getFavorites function

--- src/service/APIService.ts
+++ src/service/APIService.ts
@@ -2,11 +2,25 @@ interface FetchParams {
     url: string;
 }
 
+interface FavoriteParams {
+  favoritesList: string;
+  type: 'character' | 'location';
+}
+
 export async function getApiData ({ url }: FetchParams) {
   const response = await fetch(url)
   const data = await response.json()
   if (data.error) {
     throw new Error('Error fetching data from API')
+  }
+  return data
+}
+
+export async function getFavorites ({ favoritesList, type }: FavoriteParams) {
+  const response = await fetch(`https://rickandmortyapi.com/api/${type}/${favoritesList}`)
+  const data = await response.json()
+  if (data.error) {
+    throw new Error('Error fetching favorite characters')
   }
   return data
 }
