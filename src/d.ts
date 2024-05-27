fffde06e164b97ae67fa40f976f9521f98066b4c
AlvaroTorres0 <alvaro.espindola.torres@gmail.com>
2024-05-27 13:20:17 -0600

    refactor: Update d.ts file to include interfaces for managing favorite characters and locations

--- src/d.ts
+++ src/d.ts
@@ -30,3 +30,47 @@ export interface APIResponse {
     readonly info: Info;
     readonly results: Character[] | Location[];
 }
+
+export interface FavoritesCharacters {
+    readonly character_id: number;
+}
+
+export interface FavoritesLocations {
+    readonly location_id: number;
+}
+
+export interface SupabaseUser {
+    id: string;
+    email?: string;
+  }
+
+export interface SupabaseSession {
+    user: SupabaseUser | null;
+    access_token: string | null;
+    expires_at: string | null;
+  }
+
+export interface FavoriteCharacter {
+    readonly id: number;
+    readonly name: string;
+    readonly status: string;
+    readonly species: string;
+    readonly type: string;
+    readonly gender: string;
+    readonly origin: Location;
+    readonly location: Location;
+    readonly image: string;
+    readonly episode: string[];
+    readonly url: string;
+    readonly created: Date;
+}
+
+export interface FavoriteLocation {
+    readonly id: number;
+    readonly name: string;
+    readonly type: string;
+    readonly dimension: string;
+    readonly residents: string[];
+    readonly url: string;
+    readonly created: Date;
+}
