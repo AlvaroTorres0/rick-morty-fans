81fe333e6de4ee18ee6413675d9996d970754e95
AlvaroTorres0 <alvaro.espindola.torres@gmail.com>
2024-05-27 13:21:41 -0600

    refactor: Update getFavoritesLocationIDs and getFavoritesCharactersIDs functions to use typed parameters

--- src/utils/utilis.ts
+++ src/utils/utilis.ts
@@ -1,11 +1,11 @@
-import { Location, Character } from '../d'
+import { FavoritesCharacters, FavoritesLocations } from '../d'
 
-export function getFavoritesLocationIDs (arr: Location[]) {
+export function getFavoritesLocationIDs (arr: FavoritesLocations[]) {
   const locationIds = arr.map(obj => obj.location_id)
   return locationIds.join(',')
 }
 
-export function getFavoritesCharactersIDs (arr: Character[]) {
+export function getFavoritesCharactersIDs (arr: FavoritesCharacters[]) {
   const charactersIds = arr.map(obj => obj.character_id)
   return charactersIds.join(',')
 }
