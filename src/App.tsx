1f1378ba5c350559791c3dc0f9eae246c598b4a7
AlvaroTorres0 <alvaro.espindola.torres@gmail.com>
2024-05-27 09:53:32 -0600

    feat: Add sonner package for toast notifications

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import './App.css'
 import React from 'react'
 import RountingHandler from './RoutingHandler'
 import { AuthContextProvider } from './context/AuthContext'
+import { Toaster } from 'sonner'
 
 const App: React.FC = () => {
   return (
     <AuthContextProvider>
       <RountingHandler></RountingHandler>
+      <Toaster />
     </AuthContextProvider>
   )
 }
